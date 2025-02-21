35a520c9f0b502b357c089febfbaa72c61bd203e
Harshith Alle <alleharshith@eksaq.in>
2025-02-21 23:48:04 +0530

    useChat hook

---
+++ frontend/hooks/useChat.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const useChat = ({ api }: { api: string }) => {
+  const [messages, setMessages] = useState([]);
+	
+  return {};
+};
+
+export default useChat;
