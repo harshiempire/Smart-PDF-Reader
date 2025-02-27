ef21ee029b8243c511affe8e602b4de878993716
Harshith Alle <alleharshith@eksaq.in>
2025-02-27 21:44:44 +0530

    save

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,49 +1,85 @@
 "use client";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Home() {
+  const inputRef = useRef(null);
   const [data, setData] = useState([]);
   const [answer, setAnswer] = useState("");
+  const [streamdiv, setStreamdiv] = useState(false);
 
   const startStreamData = async () => {
-    const response = await axios.post("http://localhost:8000/stream-chat", {
-      prompt: answer,
-      history: data,
-    });
-    if (!response.ok || !response.body) {
-      throw response.statusText;
-    }
-
     const ndata = [
       ...data,
       { role: "user", parts: [{ text: inputRef.current.value }] },
     ];
 
-    const reader = response.body.getReader();
-    const txtdecoder = new TextDecoder();
-    var modelResponse = "";
-    while (true) {
-      const { value, done } = reader.read();
-      if (done) break;
-      setAnswer((prev) => {
-        prev + value;
-      });
+      let modelResponse = "";
 
-      modelResponse += value;
+      const chatData = {
+        prompt: inputRef.current.value,
+        history: 
+      }
+
+      setData(ndata);
+      inputRef.current.value = "";
+      inputRef.current.placeholder = "Waiting for model response";
+
+      const response = await axios.post(
+        "http://localhost:8000/stream-chat",
+        chatData
+      );
+      if (!response.ok || !response.body) {
+        throw response.statusText;
+      }
+
+      const ndata = [
+        ...data,
+        { role: "user", parts: [{ text: inputRef.current.value }] },
+      ];
+
+      const reader = response.body.getReader();
+      const txtdecoder = new TextDecoder();
+      while (true) {
+        const { value, done } = reader.read();
+        if (done) break;
+        setAnswer((prev) => {
+          prev + value;
+        });
+
+        modelResponse += value;
+      }
+
+      setData(updatedData);
+    } catch (err) {
+      modelResponse = "Error occurred";
+    } finally {
+      const updatedData = [
+        ...ndata,
+        { role: "model", parts: [{ text: modelResponse }] },
+      ];
+      setAnswer("");
     }
-
-    const updatedData = [
-      ...ndata,
-      { role: "model", parts: [{ text: modelResponse }] },
-    ];
-    setData(updatedData)
-
   };
 
-return (
-  <div>
-   
-  </div>
-)
+  return (
+    <div className="grid grid-flow-col grid-rows-3 gap-4 h-screen">
+      <div className="border row-span-3 ">01</div>
+      <div className="border col-span-2 row-span-2">
+        {data.map((msg, index) => {
+          return (
+            <div key={index}>
+              <div>{msg.role}</div>
+              <div>{msg.parts[0].text}</div>
+            </div>
+          );
+        })}
+        {streamdiv && <div>{answer}</div>}
+      </div>
+      <div className="border col-span-2 ">
+        <input ref={inputRef} />
+        <button onClick={startStreamData}></button>
+      </div>
+    </div>
+  );
 }
