7d86ac047e41a69066b5afab66cd8c7ec00e91c5
harshiempire <alleharshith@gmail.com>
2025-03-31 16:00:36 +0530

    save

--- backend/models.py
+++ backend/models.py
@@ -1,20 +1,29 @@
+import enum
 from typing import Optional
 from pydantic import Field, BaseModel
 from bson import ObjectId
 import datetime
 
+
 class PydanticObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
+
     @classmethod
     def validate(cls, v):
         if not ObjectId.is_valid(v):
             raise ValueError("Invalid ObjectId")
         return ObjectId(v)
 
+
 class DocumentSchema(BaseModel):
     id: Optional[PydanticObjectId] = Field(default=None, alias="_id")
     filename: str
     upload_date: datetime
+    pages: int
+    processing_status: enum
+    user_id: str
+
+class Segment
 
